77c096c2a69d03ebb31fa2dca4b01c7732b2416f
ProchaR1 <prochar1@gmail.com>
2025-03-20 10:18:07 +0100

    Update project name, add repeat button SVG, and modify success messages

--- public/content/config.js
+++ public/content/config.js
@@ -6,7 +6,7 @@ window.config = {
   width: 745,
   height: 696,
   imageUrl: './content/vlk.jpg',
-  successMessage: 'Gratulujeme, úspěšně jste složil obrázek!',
-  expireTimeoutMessage: 'Čas vypršel! Nepodařilo se vám složit obrázek.',
+  successMessage: 'Gratulujeme!',
+  expireTimeoutMessage: 'Čas vypršel!',
   repeatGameButton: 'Opakovat hru',
 };
